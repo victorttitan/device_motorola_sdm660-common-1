e8302d9468411076f39fffd61c61ba920bee698c
Maycon Eduardo <58239064+Mayc001@users.noreply.github.com>
2023-02-11 17:12:06 -0300

    Update common.mk

--- common.mk
+++ common.mk
@@ -82,7 +82,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
-    libbson.vendor \
     libxml2 \
     vendor.qti.hardware.camera.device@1.0:64
 
